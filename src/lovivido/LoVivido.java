f452223ed329bba14444c58ad28e44bb2aeda425
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-12-14 10:05:19 +0100

    Añadido Javadoc

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -6,12 +6,27 @@ package lovivido;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Esta clase contiene el metodo main del programa para calcular el tiempo que
+ * lleva vivo una persona en meses, dias y horas
+ *
+ * @author Arturo 6037
+ *
+ */
 public class LoVivido{
 
+    /**
+     * El metodo main se encarga crear un objeto de la clase CalculoTiempo y de
+     * llamar a los metodos de la clase CalculoTiempo
+     *
+     * @param args
+     *
+     */
     public static void main(String[] args){
         CalculoTiempo t=new CalculoTiempo();
-        t.CalcularTiempo();
+        String nombre=t.recogerNombre();
+        String edad=t.recogerEdad();
+        t.calcularTiempo(nombre, edad);
     }
-    
 
 }
